2a3fcecfbad9aeca89f285e707017769b6880e86
BruceICzw <h220396z@hit.ac.zw>
2023-10-01 09:42:39 +0200

    Add project files.

---
+++ UsersAPI/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using UsersAPI.Models;
+
+namespace UsersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public UserController( IConfiguration config) {
+            _config = config;
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetAllUsers()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<User> users = await SelectAllUsers(connection);
+            return Ok(users);
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<User>> GetUser(int userId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var user = await connection.QueryFirstAsync("Select * from users where id=@Id", new {Id = userId });
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser(User user)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into users(name, email, username, password) values(@Name, @Email, @Username,@Password )", user);
+            return Ok(await SelectAllUsers(connection));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<User>>> UpdateUser(User user)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("update users set name=@Name,email=@Email, username=@Username, password=@Password where id=@Id", user);
+            return Ok(await SelectAllUsers(connection));
+        }
+        [HttpDelete("{userId}")]
+        public async Task<ActionResult<List<User>>> DeleteUser(int userId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            _ = await connection.ExecuteAsync("delete from users where id = @Id", new { Id = userId });
+            return Ok(await SelectAllUsers(connection));
+        }
+        private static async Task<IEnumerable<User>> SelectAllUsers(SqlConnection connection)
+        {
+            return await connection.QueryAsync<User>("Select * from users");
+        }
+    }
+}
---
+++ UsersAPI/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace UsersAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
